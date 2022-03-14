4bac24d5cc03963970e0e9249dc0bfcd602c2517
Splines <dominic-plein@gmx.de>
2022-03-14 23:34:44 +0100

    Replace tap/swipe with one/two finger swipe gesture

--- frontend/src/gesture.js
+++ frontend/src/gesture.js
@@ -1,40 +1,24 @@
+const DIRECTION_LEFT = 2;
+const DIRECTION_HORIZONTAL = 6;
+
 function handleGestures(ws) {
     const gestureArea = document.getElementById('gesture-area');
-    const mc = new Hammer.Manager(gestureArea, {
-        recognizers: [
-            [Hammer.Tap],
-            [Hammer.Swipe, { direction: Hammer.DIRECTION_HORIZONTAL }]
-        ]
+    const mc = new Hammer.Manager(gestureArea);
+    mc.add(new Hammer.Swipe({ event: 'oneFingerHorizontalSwipe', pointers: 1, direction: DIRECTION_HORIZONTAL }));
+    mc.add(new Hammer.Swipe({ event: 'twoFingersHorizontalSwipe', pointers: 2, direction: DIRECTION_HORIZONTAL }));
+
+    mc.on("oneFingerHorizontalSwipe", event => {
+        const action = (event.direction === DIRECTION_LEFT)
+            ? "PASS_ON_CLOCKWISE" : "PASS_ON_COUNTER_CLOCKWISE";
+        console.log(`🎈 1 Finger: ${action}`);
+        ws.send(action);
     });
 
-    const THRESHOLD_TAP_SWIPE_GAP_IN_MS = 300;
-    let lastTapTime = Date.now();
-    let tapOccurred = false;
-
-    mc.on("tap", event => {
-        console.log('tap');
-        lastTapTime = Date.now();
-        tapOccurred = true;
-        setTimeout(() => tapOccurred = false, THRESHOLD_TAP_SWIPE_GAP_IN_MS);
-    });
-
-    mc.on("swipeleft swiperight", event => {
-        if (tapOccurred) {
-            const deltaTime = Date.now() - lastTapTime;
-            if (deltaTime > THRESHOLD_TAP_SWIPE_GAP_IN_MS) {
-                return;
-            }
-
-            const action = (event.type === "swipeleft")
-                ? "PASS_ON_CLOCKWISE_SKIP" : "PASS_ON_COUNTER_CLOCKWISE_SKIP";
-            console.log(`🎈⚡ Tap-Swipe: ${action}`);
-            ws.send(action);
-        } else {
-            const action = (event.type === "swipeleft")
-                ? "PASS_ON_CLOCKWISE" : "PASS_ON_COUNTER_CLOCKWISE";
-            console.log(`⚡ Swipe: ${action}`);
-            ws.send(action);
-        }
+    mc.on("twoFingersHorizontalSwipe", event => {
+        const action = (event.direction === DIRECTION_LEFT)
+            ? "PASS_ON_CLOCKWISE_SKIP" : "PASS_ON_COUNTER_CLOCKWISE_SKIP";
+        console.log(`🎈 2 Fingers: ${action}`);
+        ws.send(action);
     });
 
 }
