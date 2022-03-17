04a6580feacea518d2c5febba4b4b1aad665ca7c
Splines <dominic-plein@gmx.de>
2022-03-17 22:18:08 +0100

    Add pulsing background blob
    The "Get ready" button is removed and needs to be reincorporated soon.

--- frontend/src/main/app.js
+++ frontend/src/main/app.js
@@ -14,15 +14,15 @@
 
 // Ready button
 const readyButton = document.getElementById('ready-btn');
-readyButton.onclick = async () => {
-    try {
-        const res = await axios.post('/api/ready', { withCredentials: true });
-        console.log(res.data);
-    } catch (error) {
-        console.error(error);
-    }
-    enterFullscreen(document.documentElement);
-}
+// readyButton.onclick = async () => {
+//     try {
+//         const res = await axios.post('/api/ready', { withCredentials: true });
+//         console.log(res.data);
+//     } catch (error) {
+//         console.error(error);
+//     }
+//     enterFullscreen(document.documentElement);
+// }
 
 function handleTimeoutElimination() {
     console.log('Handle elimination timeout');
