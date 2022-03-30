2fe3d6d7aff9248a7c78df19c172f7c88af06dd8
Splines <37160523+Splines@users.noreply.github.com>
2022-03-30 17:40:41 +0200

    Fix wrong service name

--- hotspot/setup-server.sh
+++ hotspot/setup-server.sh
@@ -1,5 +1,5 @@
 sudo cp ./captivecircle.service /etc/systemd/system
-sudo systemctl enable captivecircle.service
+sudo systemctl enable captivecircle
 
 # Start service immediately
 sudo systemctl start captivecircle
